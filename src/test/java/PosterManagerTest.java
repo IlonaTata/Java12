40bcbdde6a8ab179fd3a060323c162ca04d86c66
Ilona Tatarenko <ilona-tatarenko@mail.ru>
2022-11-14 19:29:41 +0300

    Initial coomit

---
+++ src/test/java/PosterManagerTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PosterManagerTest {
+
+
+    @Test
+    public void addFilm() {
+        PosterManager manager = new PosterManager();
+        manager.addFilm("Номер 1");
+        manager.addFilm("Номер 2");
+        manager.addFilm("Номер 3");
+        manager.addFilm("Номер 4");
+        manager.addFilm("Номер 5");
+        String[] expected = {"Номер 1", "Номер 2", "Номер 3", "Номер 4", "Номер 5"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void addLastFilm() {
+        PosterManager manager = new PosterManager();
+        manager.addFilm("Номер 1");
+        manager.addFilm("Номер 2");
+        manager.addFilm("Номер 3");
+        manager.addFilm("Номер 4");
+        manager.addFilm("Номер 5");
+        String[] expected = {"Номер 5", "Номер 4", "Номер 3", "Номер 2", "Номер 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void addFilmWithLimit() {
+        PosterManager manager = new PosterManager(5);
+        manager.addFilm("Номер 1");
+        manager.addFilm("Номер 2");
+        manager.addFilm("Номер 3");
+        manager.addFilm("Номер 4");
+        manager.addFilm("Номер 5");
+        String[] expected = {"Номер 1", "Номер 2", "Номер 3", "Номер 4","Номер 5"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
