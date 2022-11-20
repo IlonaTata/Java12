265ccf0a5b310da888c53eda95ea02f75e853822
Ilona Tatarenko <ilona-tatarenko@mail.ru>
2022-11-20 10:47:35 +0300

    Change coomit

--- src/test/java/PosterManagerTest.java
+++ src/test/java/PosterManagerTest.java
@@ -1,9 +1,6 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 public class PosterManagerTest {
-
-
     @Test
     public void addFilm() {
         PosterManager manager = new PosterManager();
@@ -40,4 +37,29 @@ public class PosterManagerTest {
         String[] actual = manager.findAll();
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void addFilmOverLimit() {
+        PosterManager manager = new PosterManager(5);
+        manager.addFilm("Номер 1");
+        manager.addFilm("Номер 2");
+        manager.addFilm("Номер 3");
+        manager.addFilm("Номер 4");
+        manager.addFilm("Номер 5");
+        manager.addFilm("Номер 6");
+        String[] expected = {"Номер 6", "Номер 5", "Номер 4", "Номер 3", "Номер 2"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void addFilmLessLimit() {
+        PosterManager manager = new PosterManager(5);
+        manager.addFilm("Номер 1");
+        manager.addFilm("Номер 2");
+        manager.addFilm("Номер 3");
+        manager.addFilm("Номер 4");
+        String[] expected = {"Номер 4", "Номер 3", "Номер 2", "Номер 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 }
